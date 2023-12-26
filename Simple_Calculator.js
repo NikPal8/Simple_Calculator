c733c7550ebc18550f2e19ea0b1e9340c764a04a
Nik <nik_palibrk@hotmail.com>
2023-12-26 16:24:08 +1100

    In JavaScript, I've selected displaySceen and all buttons. For of loop was used to iterate over every button. Inside I've nested the click event listener to dsiplay specific button clicked. Created a boolean value to determine if calculator is on or off, this will be done with an if/else statement inside the loop that will change the buttons OFF text to On and the boolean variable to true. Another if statement check if calculator is on boolean value, if false the buttons doesn't return any value. Lastly I have tested using console logs that this on/off feature if on allows buttons to return value or logs and if off nothing happens.

--- Simple_Calculator.js
+++ Simple_Calculator.js
@@ -1,1 +1,34 @@
+//Selected the displayScreen element to display the numbers to user.
+//Selected the buttons and looped them using for of loop. 
+//Created Click Event listener
+//Created a variable 'isCalculatorOn' with a boolean value
+//Added if statments to checks if btn === 'OFF' /'On', button text changes to 'OFF'/ 'On'
+//I have attached the isCalculatorOn to the if statements, to change true to false
+//This value will if false will prevent user from getting values of buttons clicked. 
 
+const displayScreen = document.querySelector('#displayScreen');
+const buttons = document.querySelectorAll('button');
+
+let isCalculatorOn = false;
+
+for (const button of buttons) {
+     button.addEventListener('click', function() {
+          let btn = button.textContent;
+
+          if (btn === 'OFF') {
+               button.textContent = 'On';
+               isCalculatorOn = true;
+               console.log(btn);
+               console.log("isCalculatorOn", isCalculatorOn);
+          } else if (btn === 'On') {
+               button.textContent = 'OFF';
+               isCalculatorOn = false;
+               console.log(btn);
+               console.log("isCalculatorOn", isCalculatorOn);
+          }
+
+          if (isCalculatorOn === true) {
+               console.log(btn);
+          }
+     })
+}
