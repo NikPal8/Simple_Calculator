45747fddcda238f3fb5ed481d37bbb6eb514601d
Nik <nik_palibrk@hotmail.com>
2023-12-28 22:42:52 +1100

    I followed the same process when creating the add feature as I did with the other arithmetic -, *, /. I was unable to figure out a way to increment when clicking + without doubling the initial number. Currently to calculate 2 values the equals button needs to be clicked. Last note I added a clear button feature that resets users inputs.

--- Simple_Calculator.js
+++ Simple_Calculator.js
@@ -1,18 +1,12 @@
 //NOTES:
-//Date: 26/12/2023
-//Selected the displayScreen element to display the numbers to user.
-//Selected the buttons and looped them using for of loop. 
-//Created Click Event listener
-//Created a variable 'isCalculatorOn' with a boolean value
-//Added if statments to checks if btn === 'OFF' /'On', button text changes to 'OFF'/ 'On'
-//I have attached the isCalculatorOn to the if statements, to change true to false
-//This value will if false will prevent user from getting values of buttons clicked. 
 //Date:27/12/2023
 //Created an if statement to check if current button clicked is a number using !isNaN function.
 //If a number is a number created a variable currentNumber to add to buttons clicked and hold that value as a string.
 //The displayScreen var shows currentNumber.
 //Created another if statement using .includes to check button text value matches '+', '-', 'x', '/'.
 //Another variable was created to hold the currentNumber value, then currentNumber = ''. This was done to stop concatinating to later add different values.
+//Used the same steps for the + if statement on the mins, times and divide.
+
 const displayScreen = document.querySelector('#displayScreen');
 const buttons = document.querySelectorAll('button');
 
@@ -20,6 +14,7 @@ let isCalculatorOn = false;
 let currentNumber = '';
 let result = 0;
 let operator = null;
+let calculation = 0;
 
 
 for (const button of buttons) {
@@ -29,40 +24,79 @@ for (const button of buttons) {
           if (btnText === 'OFF') {
                button.textContent = 'On';
                isCalculatorOn = true;
-               console.log("btnText", btnText);
-               console.log("isCalculatorOn", isCalculatorOn);
+               // console.log("btnText", btnText);
+               // console.log("isCalculatorOn", isCalculatorOn);
+
           } else if (btnText === 'On') {
                button.textContent = 'OFF';
                isCalculatorOn = false;
-               console.log("btnText", btnText);
-               console.log("isCalculatorOn", isCalculatorOn);
+               // console.log("btnText", btnText);
+               // console.log("isCalculatorOn", isCalculatorOn);
           }
 
           if (isCalculatorOn === true) {
                if (!isNaN(btnText)) {
                     currentNumber = currentNumber + btnText;
-                    console.log("btnText", btnText);
-                    console.log("currentNumber", currentNumber);
+                    // console.log("btnText", btnText);
+                    // console.log("currentNumber", currentNumber);
                     displayScreen.textContent = currentNumber;
-                    console.log("operator", operator);
-               } else if (['+', '-', 'x', '/'].includes(btnText)) {
-                    operator = btnText;
-                    result = currentNumber;
-                    console.log("result", result);
-                    currentNumber = '';
-
+                    // console.log("operator", operator);
+                    
+                    
+               
+               }  else if (['+', '-', 'x', '/'].includes(btnText)) {
+                    
+                    if (operator === '+') {
+                         console.log("what is result:", result);
+                         console.log("what is currentNumber:", currentNumber);
+                         btnText.textContent = '=';
+                         // btnText = '=' 
+                         // console.log("btnText", btnText);
+                         currentNumber = '';
+                    } else {
+                         
+                         // console.log("this result", result);
+                         operator = btnText;
+                         // console.log("operator", operator);
+                         result = currentNumber;
+                         // console.log("result", result);
+                         currentNumber = '';
+                    }
+               
+                    
+                    
+                    
                } else if (btnText === '=') {
+                    console.log("before + :currentNumber", currentNumber);
+                    console.log("before + :result", result);
                     if (operator === '+') {
                          result = parseInt(result) + parseInt(currentNumber);
-                         
                          console.log("result", result);
+                         
+
+                    } else if (operator === '-') {
+                         result = parseInt(result) - parseInt(currentNumber);
+
+                    } else if (operator === 'x') {
+                         result = parseInt(result) * parseInt(currentNumber);
+
+                    } else if (operator === '/') {
+                         result = parseInt(result) / parseInt(currentNumber);
+
                     }
+                    // console.log("result", result);
                     currentNumber = result;
                     displayScreen.textContent = currentNumber;
+                    // console.log("currentNumber", currentNumber);
                }
 
 
 
+          } else if (btnText === 'C') { //clears all values
+               result = 0;
+               currentNumber = '';
+               displayScreen.textContent = "";
+               operator = null;
           }
      })
 }
